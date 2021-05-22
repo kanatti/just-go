30036f8cd5b4fee6ddd6c40af810165409f627bb
kanatti <balukanattil@gmail.com>
2021-05-22 12:25:29 +0530

    Add view templates and fix route mux

--- books/go-blueprints/chat/main.go
+++ books/go-blueprints/chat/main.go
@@ -1,50 +1,85 @@
 package main
 
 import (
+	"html/template"
 	"log"
 	"net/http"
+	"os"
+	"path/filepath"
+	"sync"
+
+	"github.com/gorilla/mux"
 )
 
+func getProjectPath() string {
+	currentDir, _ := os.Getwd()
+	return filepath.Join(currentDir, "books", "go-blueprints", "chat")
+}
+
+type handlerMiddleWare func(res http.ResponseWriter, req *http.Request)
+
+// Handler that responds with templates
+type templateHandler struct {
+	preWriter handlerMiddleWare
+	filename  string
+	once      sync.Once
+	templ     *template.Template
+}
+
+// ServeHttp for Handler interface
+func (t *templateHandler) ServeHTTP(res http.ResponseWriter, req *http.Request) {
+	// Do a lazy parsing of the template once
+	t.once.Do(func() {
+		log.Print("Compiling template: ", t.filename)
+		templatePath := filepath.Join(getProjectPath(), "templates", t.filename)
+		log.Print("Template path is ", templatePath)
+		t.templ = template.Must(template.ParseFiles(templatePath))
+	})
+
+	// Prewriter Middleware
+	if t.preWriter != nil {
+		log.Print("Executing prewriter")
+		t.preWriter(res, req)
+	} else {
+		log.Print("No prewriter, skipping")
+	}
+
+	// Write template response
+	if t.templ != nil {
+		log.Print("Writing template response")
+		t.templ.Execute(res, nil)
+	} else {
+		log.Print("No compiled template found")
+	}
+}
+
 func main() {
-	http.HandleFunc("/", func(res http.ResponseWriter, req *http.Request) {
-		res.Header().Add("SERVER", "GoLang")
-		res.Write([]byte(`
-		<html>
-			<head>
-			<title>Chat</title>
-			</head>
-			<body>
-			Let's chat!
-			</body>
-		</html> `))
-	})
-	http.HandleFunc("/about", func(res http.ResponseWriter, req *http.Request) {
-		res.Write([]byte(`
-		<html>
-			<head>
-			<title>About</title>
-			</head>
-			<body>
-			Nothing Here
-			</body>
-		</html> `))
-	})
-	http.HandleFunc("/redirect", func(res http.ResponseWriter, req *http.Request) {
+	chatHandler := &templateHandler{
+		filename: "chat.html",
+		preWriter: func(res http.ResponseWriter, req *http.Request) {
+			res.Header().Add("SERVER", "GoLang")
+		},
+	}
+
+	aboutHandler := &templateHandler{filename: "about.html"}
+
+	pageNotFoundHandler := &templateHandler{
+		filename: "404.html",
+		preWriter: func(res http.ResponseWriter, req *http.Request) {
+			res.WriteHeader(404)
+		},
+	}
+
+	router := mux.NewRouter()
+	router.Handle("/", chatHandler)
+	router.Handle("/404", pageNotFoundHandler)
+	router.Handle("/about", aboutHandler)
+	router.HandleFunc("/redirect", func(res http.ResponseWriter, req *http.Request) {
 		res.Header().Add("Location", "/about")
 		res.WriteHeader(302)
 	})
-	http.HandleFunc("/404", func(res http.ResponseWriter, req *http.Request) {
-		res.WriteHeader(404)
-		res.Write([]byte(`
-		<html>
-			<head>
-			<title>404</title>
-			</head>
-			<body>
-			Page Not Found
-			</body>
-		</html> `))
-	})
+	http.Handle("/", router)
+
 	log.Print("Serving on: ", 3000)
 	if err := http.ListenAndServe("localhost:3000", nil); err != nil {
 		log.Fatal("ListenAndServe err:", err)
--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/kanatti/just-go
 
 go 1.15
+
+require (
+	github.com/gorilla/mux v1.8.0
+	golang.org/x/time v0.0.0-20210220033141-f8bda1e9f3ba
+)
